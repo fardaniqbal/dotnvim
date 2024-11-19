d94f7864feb12b81e563514a0dfebf76ba1f9e7c
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-11-19 12:21:21 -0500

    load vim config from ~/.vimrc or ~/.vim/vimrc if available

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
---require("plugins")
+-- Include vim configuration if available.  Based on
+-- https://neovim.io/doc/user/nvim.html#nvim-from-vim, translated to Lua.
+vim.opt.runtimepath:prepend({ vim.fn.expand('~/.vim') })
+vim.opt.runtimepath:append({ vim.fn.expand('~/.vim/after') })
+vim.cmd("let &packpath = expand(&runtimepath)")
+if (vim.fn.filereadable(vim.fn.expand('~/.vimrc')) ~= 0) then
+  vim.cmd("source ~/.vimrc")
+elseif (vim.fn.filereadable(vim.fn.expand('~/.vim/vimrc')) ~= 0) then
+  vim.cmd("source ~/.vim/vimrc")
+end
+
 require("fardan")
