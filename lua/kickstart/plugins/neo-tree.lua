e8deb68ce46b64595306c6a1882534957eb66239
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-11-25 19:19:40 -0500

    neo-tree.lua: hide dotfiles and gitignore files by default

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -168,8 +168,8 @@ return {
       filesystem = {
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = false,
-          hide_gitignored = false,
+          hide_dotfiles = true,
+          hide_gitignored = true,
           hide_hidden = false, -- only works on Windows for hidden files/directories
           hide_by_name = {
             --"node_modules"
