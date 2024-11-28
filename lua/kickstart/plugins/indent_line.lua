6ef4444244665057db9945340fb01fd31213a3a8
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-11-28 01:36:05 -0500

    tweak indent_line's scope highlight

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,17 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    event = 'VeryLazy',
+    opts = {
+      scope = {
+        show_start = false, -- if true, underline the scope's first line
+        show_end = false,   -- if true, underline the scope's last line
+
+        -- HACK: change scope highlight color by just setting it to another
+        -- highlight group.  Definitely NOT the right way to do this, but I
+        -- have bigger fish to fry.
+        highlight = {'diffLine'}
+      },
+    },
   },
 }
