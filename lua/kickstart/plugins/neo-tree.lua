040916c700a3be9143f25f13b52b1f730e5afe68
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-11-29 13:50:09 -0500

    show git status and diagnostics in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -33,6 +33,10 @@ return {
       -- Close Neo-tree if it is the last window left in the tab.
       close_if_last_window = true,
       popup_border_style = 'rounded',
+      enable_git_status = true,
+      enable_diagnostics = true,
+      -- when opening files, do not use windows containing these filetypes or buftypes
+      open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
       -- Used when sorting files and directories in the tree.
       sort_case_insensitive = true,
       default_component_configs = {
