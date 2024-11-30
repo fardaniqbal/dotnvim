891115b8539c9c0ad796724d2f988fb816dc6939
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-11-30 10:40:08 -0500

    new keybind: <leader>bd to close current buffer without changing window layout

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -1,14 +1,17 @@
 -- Custom key bindings.
 
 -- Tabs.
-vim.keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', {desc = '[T]ab: [O]pen tab'})
-vim.keymap.set('n', '<leader>tc', '<cmd>tabclose<CR>', {desc = '[T]ab: [C]lose tab'})
-vim.keymap.set('n', '<leader>tn', '<cmd>tabnext<CR>', {desc = '[T]ab: [N]ext tab'})
-vim.keymap.set('n', '<leader>tp', '<cmd>tabprevious<CR>', {desc = '[T]ab: [P]revious tab'})
+vim.keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', {desc = '[T]ab [O]pen'})
+vim.keymap.set('n', '<leader>tc', '<cmd>tabclose<CR>', {desc = '[T]ab [C]lose'})
+vim.keymap.set('n', '<leader>tn', '<cmd>tabnext<CR>', {desc = '[T]ab [N]ext'})
+vim.keymap.set('n', '<leader>tp', '<cmd>tabprevious<CR>', {desc = '[T]ab [P]revious'})
 
 -- Buffers.
-vim.keymap.set('n', '<leader>bn', '<cmd>bnext<CR>', {desc = '[B]uffer: [N]ext'})
-vim.keymap.set('n', '<leader>bp', '<cmd>bprevious<CR>', {desc = '[B]uffer: [P]revious'})
+vim.keymap.set('n', '<leader>bn', '<cmd>bnext<CR>', {desc = '[B]uffer [N]ext'})
+vim.keymap.set('n', '<leader>bp', '<cmd>bprevious<CR>', {desc = '[B]uffer [P]revious'})
+
+-- Like :bd[elete], but doesn't change the window layout.
+vim.keymap.set('n', '<leader>bd', '<cmd>b#<bar>bd#<CR>', {desc = '[B]uffer [D]elete'})
 
 -- Make vim-tmux-navigator work in Vim's built-in terminal emulator.
 vim.keymap.set('t', '<c-h>', '<cmd>TmuxNavigateLeft<cr>')
