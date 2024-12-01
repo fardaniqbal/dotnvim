0e05a99b48ee6c69f96fc4fc1f0a00151ae4e90a
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-12-01 15:37:56 -0500

    map <leader>bd to :Bclose in bclose.vim

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -10,8 +10,8 @@ vim.keymap.set('n', '<leader>tp', '<cmd>tabprevious<CR>', {desc = '[T]ab [P]revi
 vim.keymap.set('n', '<leader>bn', '<cmd>bnext<CR>', {desc = '[B]uffer [N]ext'})
 vim.keymap.set('n', '<leader>bp', '<cmd>bprevious<CR>', {desc = '[B]uffer [P]revious'})
 
--- Like :bd[elete], but doesn't change the window layout.
-vim.keymap.set('n', '<leader>bd', '<cmd>b#<bar>bd#<CR>', {desc = '[B]uffer [D]elete'})
+-- Like :bd[elete], but doesn't change the window layout.  Uses blcose.vim.
+vim.keymap.set('n', '<leader>bd', '<cmd>Bclose<CR>', {desc = '[B]uffer [D]elete'})
 
 -- Make vim-tmux-navigator work in Vim's built-in terminal emulator.
 vim.keymap.set('t', '<c-h>', '<cmd>TmuxNavigateLeft<cr>')
