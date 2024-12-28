64061ca8313165d76cf943be18a2d3faee87a95a
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-12-28 14:59:16 -0500

    fix: smear-cursor.lua: don't temporarily hide previous char when moving cursor

--- lua/custom/plugins/smear-cursor.lua
+++ lua/custom/plugins/smear-cursor.lua
@@ -3,6 +3,10 @@ return {
   "sphamba/smear-cursor.nvim",
   event = 'VeryLazy',
   opts = {
-    enabled = vim.fn.exists('g:neovide') == 0 -- smear is built in to neovide
+    enabled = vim.fn.exists('g:neovide') == 0, -- smear is built in to neovide
+
+    -- Stop animating when the smear's tail is within this distance (in
+    -- characters) from the target.  Default = 0.1.
+    distance_stop_animating = 1.5,
   },
 }
