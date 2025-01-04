886b0ad26234aee70f0615a4f404cd2f1a27b9c6
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2025-01-04 14:48:03 -0500

    convert ginit.vim to ginit.lua

---
+++ ginit.lua
@@ -0,0 +1,3 @@
+if vim.g.neovide then
+  vim.o.guifont = "MesloLGS_Nerd_Font,CommitMono_Nerd_Font,CaskaydiaMono_Nerd_Font,Hack_Nerd_Font,Consolas:h10"
+end
--- ginit.vim
+++
@@ -1,3 +0,0 @@
-if exists('g:neovide')
-  set guifont=MesloLGS_Nerd_Font,CommitMono_Nerd_Font,CaskaydiaMono_Nerd_Font,Hack_Nerd_Font,Consolas:h10
-endif
