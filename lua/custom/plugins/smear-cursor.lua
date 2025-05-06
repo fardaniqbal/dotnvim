b99ed8e03c2f4405d02b05cd082e18283e09cef0
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2025-05-06 16:42:05 -0400

    disable smear-cursor if running over SSH (it's too slow)

--- lua/custom/plugins/smear-cursor.lua
+++ lua/custom/plugins/smear-cursor.lua
@@ -1,10 +1,21 @@
 -- Cursor smearing animations to make large cursor movements easier to see.
+
+local function has_env(varname)
+  local val = os.getenv(varname)
+  return val ~= nil and val ~= ''
+end
+
 return {
   "sphamba/smear-cursor.nvim",
   event = 'VeryLazy',
-  opts = {
-    enabled = vim.fn.exists('g:neovide') == 0, -- smear is built in to neovide
 
+  -- Disable this plugin if using Neovide (Neovide has it built in), or if
+  -- running over SSH (this plugin can be very laggy over slow SSH).
+  cond = (vim.fn.exists('g:neovide') == 0 and
+          not has_env('SSH_CLIENT') and
+          not has_env('SSH_CONNECTION') and
+          not has_env('SSH_TTY')),
+  opts = {
     -- Stop animating when the smear's tail is within this distance (in
     -- characters) from the target.  Default = 0.1.
     distance_stop_animating = 1.5,
