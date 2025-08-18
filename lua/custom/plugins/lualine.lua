145781ebed33c0e60f543fe5c2042ce1d3613cf8
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2025-08-18 00:35:25 -0400

    lualine.lua: improve debounce logic with uv.new_timer()
    Gives us lower CPU usage and better responsiveness.  Nvim still
    occasionally shows non-zero CPU usage while sitting idle, but does so
    _even with lualine disabled_.  **Lualine is no longer the bottleneck.**

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,12 +1,69 @@
 -- Custom statusline using lualine.nvim.  For customization options see
 -- https://github.com/nvim-lualine/lualine.nvim
+
+---- Custom Refresh Setup ----
+-- Though lualine is quite responsive by default, its default refresh_time
+-- of 16 milliseconds makes nvim eat up noticable CPU even while idle.
+-- Therefore, we use this workaround to force lualine to refresh on certain
+-- events so it stays responsive, even with low refresh rate settings.
+local setup_refresh_timer = function()
+  -- XXX: this should more-or-less match lualine's default config values
+  -- for options.refresh.events.  It's a pain, but we'll have to keep this
+  -- up-to-date with lualine's default options in the README:
+  -- https://github.com/nvim-lualine/lualine.nvim/blob/master/README.md#global-options
+  --
+  -- This _does not_ mean that this list of events should match _exactly_
+  -- with the above lualine docs.  But it _does_ mean we should pay
+  -- attention if it changes on future lualine updates.
+  local LUALINE_REFRESH_EVENTS = {
+    'WinEnter',
+    'BufEnter',
+    'BufWritePost',
+    'SessionLoadPost',
+    'FileChangedShellPost',
+    'VimResized',
+    'Filetype',
+    "CursorMoved",
+    "CursorMovedI",
+    "ModeChanged",
+  }
+  local REFRESH_DEBOUNCE_TIME = 20  -- minimum milliseconds between refreshes
+
+  local lualine = require('lualine')
+  local uv = vim.uv or vim.loop
+  local dbg_refresh_cnt = 0
+
+  local refresh_callback = function(ev)
+    if false then print('('..ev.event..') refreshes: '..dbg_refresh_cnt) end
+    dbg_refresh_cnt = dbg_refresh_cnt + 1
+    lualine.refresh({
+      force = true,
+      -- 'place' must have one or more of: 'statusline', 'winbar', 'tabline'.
+      -- Add these values here if you enable winbar or tabline.
+      place = { 'statusline', },
+    })
+  end
+
+  local debounce_timer = uv.new_timer()
+  vim.api.nvim_create_autocmd(LUALINE_REFRESH_EVENTS,
+    {
+      group = vim.api.nvim_create_augroup("MyLualineForceRefreshGroup", { clear = true }),
+      callback = function(ev)
+        assert(debounce_timer ~= nil)
+        debounce_timer:start(REFRESH_DEBOUNCE_TIME, 0, function()
+          debounce_timer:stop()
+          vim.schedule(function() refresh_callback(ev) end)
+        end)
+      end,
+    }
+  )
+end
+
+---- Main Lualine Plugin Config ----
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local lualine = require('lualine')
-    local uv = vim.uv or vim.loop
-
     -- Custom component 'my_filetype' - like Lualine's built-in 'filetype'
     -- component, but use Unicode icon instead of nerdfont icon.  Don't
     -- want to make coworkers install a nerdfont just for tmux sharing.
@@ -14,49 +71,7 @@ return {
       return '⠿ ' .. vim.bo.filetype
     end
 
-    -- Workaround to force lualine to refresh on certain events so it stays
-    -- responsive, even with low refresh rate settings.
-    local last_refresh_time = uv.now()
-    local refresh_debounce = 16 -- minimum milliseconds between refreshes
-    vim.api.nvim_create_autocmd(
-    {
-      -- XXX: this should more-or-less match lualine's default config
-      -- values for options.refresh.events.  It's a pain, but we'll have to
-      -- keep this up-to-date with lualine's default options in the README:
-      -- https://github.com/nvim-lualine/lualine.nvim/blob/master/README.md#global-options
-      --
-      -- This _does not_ mean that this list of events should match
-      -- _exactly_ with the above lualine docs.  But it _does_ mean we
-      -- should pay attention if it changes on future lualine updates.
-      'WinEnter',
-      'BufEnter',
-      'BufWritePost',
-      'SessionLoadPost',
-      'FileChangedShellPost',
-      'VimResized',
-      'Filetype',
-      "CursorMoved",
-      "CursorMovedI",
-      "ModeChanged",
-    },
-    {
-      group = vim.api.nvim_create_augroup("MyLualineForceRefreshGroup", { clear = true }),
-      callback = function()
-        local now = uv.now()
-        local diff = now - last_refresh_time
-
-        -- XXX: uv.now() is _not_ guaranteed to be monotonic, so we must
-        -- account for it going backwards.
-        local force_refresh = diff >= refresh_debounce or diff < 0
-        if force_refresh then last_refresh_time = now end
-        lualine.refresh({
-          force = force_refresh,
-          place = { 'statusline', },
-        })
-      end,
-    })
-
-    lualine.setup {
+    require('lualine').setup {
     options = {
       icons_enabled = true,
       theme = 'auto',
@@ -66,11 +81,11 @@ return {
       section_separators = { left = ' ', right = ''},
       always_show_tabline = true,
       --globalstatus = true,  -- show one statusline for all windows if true
-      refresh = {           -- refresh times are in milliseconds
-        statusline = 500,   -- default 100
-        tabline = 500,      -- default 100
-        winbar = 500,       -- default 100
-        refresh_time = 200  -- default 16
+      refresh = {             -- refresh times are in milliseconds
+        statusline = 10000,   -- default 100
+        tabline = 10000,      -- default 100
+        winbar = 10000,       -- default 100
+        refresh_time = 10000  -- default 16
       },
       disabled_filetypes = {
         statusline = {'neo-tree'},
@@ -101,5 +116,7 @@ return {
     },
     --]]
     }
+
+    setup_refresh_timer()
   end
 }
